39b2953e17b5ec3edd707588a48cdd08d131c293
strahjuanpablo@gmail.com <strahjuanpablo@gmail.com@5b8fc1cd-bbc6-895c-4cbb-4bf482e1edf0>
2013-11-07 15:07:25 +0000

    agregue puntos jaja

--- trunk/src/model/ClienteSinPermiso.java
+++ trunk/src/model/ClienteSinPermiso.java
@@ -4,7 +4,7 @@ public class ClienteSinPermiso extends ClienteState {
 
 	@Override
 	public void solicitarPrestamo(Cliente c, int monto, int cuotas) {
-		System.out.println("Usted no puede pedir ningun prestamo en este momento");
+		System.out.println("Usted no puede pedir ningun prestamo en este momento.....");
 	}
 
 }
