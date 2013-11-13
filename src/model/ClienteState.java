d9efb8754750274ada79c01c681868469f4e05b0
angeles.tellaarena@gmail.com <angeles.tellaarena@gmail.com@5b8fc1cd-bbc6-895c-4cbb-4bf482e1edf0>
2013-11-13 00:09:46 +0000

    carpetas extra

--- src/model/ClienteState.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public abstract class ClienteState {
-	
-	public abstract void solicitarPrestamo(Cliente c, int monto, int cuotas);
-}
--- src/model/TEM.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public class TEM extends ConfiguracionGeneral{
-
-	//
-}
